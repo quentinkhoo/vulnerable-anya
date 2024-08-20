0c7e137042fcca160ac8b1e36771ca3ae67db8cb
quentinkhoo <quentin.khoo@ascendaloyalty.com>
2024-08-20 16:58:13 +0800

    ermmmm yea, i guess

--- app/controllers/tenant1/dashboard_controller.rb
+++ app/controllers/tenant1/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Tenant1::DashboardController < Tenant1Controller
   skip_before_action :authenticate_request, only: [:index]
-  
+
   def index
     @users = User.all
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,25 +13,57 @@ Tenant.destroy_all
 
 # Create Tenant1 Data
 tenant1 = Tenant.create!(name: "#{ENV.fetch("TENANT1_NAME")}")
+
+# Create Tenant1 Users
 User.create!(
-    email: "#{ENV.fetch("TENANT1_USER")}@#{ENV.fetch("TENANT1_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
-    name: "#{ENV.fetch("TENANT1_USER")}",
+    email: "#{ENV.fetch("TENANT1_USER1")}@#{ENV.fetch("TENANT1_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT1_USER1")}",
     password_digest: "#{User.digest(ENV.fetch("TENANT1_USER1_PASSWORD"))}",
+    tenant_id: tenant1.id
+)
+User.create!(
+    email: "#{ENV.fetch("TENANT1_USER2")}@#{ENV.fetch("TENANT1_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT1_USER2")}",
+    password_digest: "#{User.digest(ENV.fetch("TENANT1_USER2_PASSWORD"))}",
+    tenant_id: tenant1.id
+)
+User.create!(
+    email: "#{ENV.fetch("TENANT1_USER3")}@#{ENV.fetch("TENANT1_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT1_USER3")}",
+    password_digest: "#{User.digest(ENV.fetch("TENANT1_USER3_PASSWORD"))}",
+    tenant_id: tenant1.id
+)
+User.create!(
+    email: "#{ENV.fetch("TENANT1_USER4")}@#{ENV.fetch("TENANT1_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT1_USER4")}",
+    password_digest: "#{User.digest(ENV.fetch("TENANT1_USER4_PASSWORD"))}",
     tenant_id: tenant1.id
 )
 
 # Create Tenant2 Data
 tenant2 = Tenant.create!(name: "#{ENV.fetch("TENANT2_NAME")}")
 User.create!(
-    email: "#{ENV.fetch("TENANT2_USER")}@#{ENV.fetch("TENANT2_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
-    name: "#{ENV.fetch("TENANT2_USER")}",
+    email: "#{ENV.fetch("TENANT2_USER1")}@#{ENV.fetch("TENANT2_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT2_USER1")}",
+    password_digest: "#{User.digest(ENV.fetch("TENANT2_USER1_PASSWORD"))}",
+    tenant_id: tenant2.id
+)
+User.create!(
+    email: "#{ENV.fetch("TENANT2_USER2")}@#{ENV.fetch("TENANT2_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT2_USER2")}",
     password_digest: "#{User.digest(ENV.fetch("TENANT2_USER2_PASSWORD"))}",
     tenant_id: tenant2.id
 )
+User.create!(
+    email: "#{ENV.fetch("TENANT2_USER3")}@#{ENV.fetch("TENANT2_NAME")}#{ENV.fetch("SUBROOT_DOMAIN")}#{ENV.fetch("ROOT_DOMAIN")}".downcase,
+    name: "#{ENV.fetch("TENANT2_USER3")}",
+    password_digest: "#{User.digest(ENV.fetch("TENANT2_USER3_PASSWORD"))}",
+    tenant_id: tenant2.id
+)
 
-# # ENABLE ROW LEVEL SECURITY
-# sql = """ ALTER TABLE users ENABLE ROW LEVEL SECURITY;
-#           ALTER TABLE users FORCE ROW LEVEL SECURITY;
-#           CREATE POLICY tenant_isolation_policy ON users USING (tenant_id = current_setting('app.current_tenant_id')::UUID);
-#       """
-# ActiveRecord::Base.connection.execute(sql)
+# ENABLE ROW LEVEL SECURITY
+sql = """ ALTER TABLE users ENABLE ROW LEVEL SECURITY;
+          ALTER TABLE users FORCE ROW LEVEL SECURITY;
+          CREATE POLICY tenant_isolation_policy ON users USING (tenant_id = current_setting('app.current_tenant_id')::UUID);
+      """
+ActiveRecord::Base.connection.execute(sql)
