8686420bd425920910f04c7e7d5e308139830184
quentinkhoo <quentin.khoo@ascendaloyalty.com>
2024-08-27 18:00:36 +0800

    add show route for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,16 @@
 class Api::UsersController < ApplicationController
   def index
     @users = User.select(:id, :name, :email).all
-    puts @users.inspect
     render json: @users, status: :ok
   end
 
   def show
-    render json: @user, status: :ok
+    begin
+      @user = User.select(:id, :name, :email).find(params[:id])
+      render json: @user, status: :ok
+    rescue
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   constraints subdomain: "api#{ENV.fetch("SUBROOT_DOMAIN")}".downcase do
     namespace :api, path: "" do
       get "/users", to: "users#index", as: "users"
+      get "/user/:id", to: "users#show", as: "user"
     end
   end
 
