85ff67eff4a8c6deb26b647db17515c0968e3378
quentinkhoo <quentin.khoo@ascendaloyalty.com>
2024-08-27 17:52:20 +0800

    okay should be ready

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,30 +1,12 @@
 class Api::UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.select(:id, :name, :email).all
+    puts @users.inspect
     render json: @users, status: :ok
   end
 
   def show
     render json: @user, status: :ok
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    unless @user.update(user_params)
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user.destroy
   end
 
 end
