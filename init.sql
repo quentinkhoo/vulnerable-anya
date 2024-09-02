6404793027f09d93782295015795a121b9674c39
quentinkhoo <quentin.khoo@ascendaloyalty.com>
2024-09-02 16:23:29 +0800

    make password super secure

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 -- Some Initialization
-create user anya with password '<REDACTED_PASSWORD>';
+create user anya with password '4nya_5ECUr3_The_wor1D';
 alter database vulnerable_anya owner to anya;
 grant all privileges on database vulnerable_anya to anya;
